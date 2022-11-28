397e3b379c135abfda55b7c7c3c7cec88bb6cf55
edgar_lopez <edgarlopezacevedo@gmail.com>
2022-11-28 00:16:06 -0500

    first_mobile

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -5,22 +5,27 @@ const WeatherCard = ({ weather, temperature, isCelsius, changeUnitTemp }) => {
     <article className="weatherCard">
       <h1>Weather App</h1>
       <h3>{`${weather.name}, ${weather.sys.country}`}</h3>
+      <div>
+      <p>{isCelsius? `${temperature.celsius} °C`: `${temperature.fahrenheit} °F`} </p>
+      <button className="btn_ChangeUnity"onClick={changeUnitTemp}>Change °F/°C</button>
+      </div>
       <section>
-        <div>
+        <div className="weatherCard_img">
           <img
             src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`}
             alt=""
           />
         </div>
-        <ul>
+        
+  
+        <ul className="weatherCard_details">
           <li>{weather.weather[0].description}</li>
           <li>Wind speed: {weather.wind.speed} m/s</li>
           <li>Clouds: {weather.clouds.all} %</li>
           <li>Pressure: {weather.main.pressure} hPa</li>
         </ul>
       </section>
-      <p>{isCelsius? `${temperature.celsius} °C`: `${temperature.fahrenheit} °F`} </p>
-      <button onClick={changeUnitTemp}>Degrees °F/°C</button>
+      
     </article>
   );
 };
